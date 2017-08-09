80c6ec0d04ec33485842218782fe3a8769aa276c
rishi706 <rishi.roy706@gmail.com>
2017-08-09 14:24:12 +0100

    Add files via upload

---
+++ DBconnect.java
@@ -0,0 +1,31 @@
+package database_console.connect;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBconnect {
+	
+	
+	public static void main(String[] args){
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			String host = "jdbc:mysql://192.168.99.100:3306/db_grad_cs_1917";
+			String username = "root";
+			String password = "ppp";
+			Connection con = DriverManager.getConnection(host,username,password);
+			//System.out.println( con.getCatalog() );
+			
+		}
+		catch(SQLException err){
+			err.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
